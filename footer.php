f211972ba49b5c5ffbc0d54e248d4067b9b5704d
Vinicius Cainelli dos Santos <vinicius.santos@buscapecompany.com.br>
2014-01-06 18:01:09 -0200

    add about

--- footer.php
+++ footer.php
@@ -3,6 +3,15 @@
 
 	<footer class="site-footer">
 		<div class="wrap">
+			<p class="text-about">
+				<strong>Uma Banda Por Dia</strong> é uma iniciativa de mostrar bandas clássicas, esquecidas, novas, ou pouco conhecidas ao público!
+
+					<br><br>
+        
+            <a href="https://www.facebook.com/UmaBandaPorDia" class="link-social link-facebook">facebook.com/UmaBandaPorDia</a>
+            <a href="https://twitter.com/1bandapordia" class="link-social link-twitter">twitter.com/1bandapordia</a>
+
+			</p>
 			<p class="text-copyright">
 						<?php echo date('Y') ?> &copy; <?php bloginfo('name'); ?> <br>
 			Feito com <a href="http://wordpress.org">WordPress</a> &bull; Desenvolvido por <a href="http://designtocode.com.br">Design to Code</a>
