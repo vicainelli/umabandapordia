e71ff5cbc61de08a62c6b8a51a6974ff73f69fad
Vinicius Cainelli <vicainelli@gmail.com>
2014-01-06 07:10:11 -0200

    create single file

---
+++ single.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+			<?php
+				while ( have_posts() ) : the_post();
+
+					get_template_part( 'content', 'single' );
+
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				endwhile;
+			?>
+
+<?php get_footer(); ?>
