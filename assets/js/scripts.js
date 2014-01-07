202a8b1be3b5aa3d7eb91c260b849025edf6770d
Vinicius Cainelli dos Santos <vinicius.santos@buscapecompany.com.br>
2014-01-07 18:08:04 -0200

    script for social links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
-	
+	// Add class depend href link
+	$('.list-social a[href*="facebook.com"]').addClass('link-facebook');
+	$('.list-social a[href*="grooveshark.com"]').addClass('link-grooveshark');
+	$('.list-social a[href*="instagram.com"]').addClass('link-instagram');
+	$('.list-social a[href*="soundcloud.com"]').addClass('link-soundcloud');
+	$('.list-social a[href*="twitter.com"]').addClass('link-twitter');
+	$('.list-social a[href*="youtube.com"]').addClass('link-youtube');
 
 });
